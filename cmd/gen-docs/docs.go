f4ece4028a3e8b37bd3960d655aa0330a2aa9a59
Clement Sam <clementsam75@gmail.com>
2020-08-10 07:02:06 +0000

    Fix(docs): fixed docs autogenerator

--- cmd/gen-docs/docs.go
+++ cmd/gen-docs/docs.go
@@ -1,37 +1,14 @@
 package main
 
 import (
+	"log"
+
 	"github.com/spf13/cobra/doc"
 	"glab/commands"
-	"log"
-	"path"
-	"strings"
 )
 
-/*
-const fmTemplate = `---
-layout: page
-title: "%s"
----
-`
- */
-
 func main() {
-	filePrepender := func(filename string) string {
-		/*
-		name := filepath.Base(filename)
-		base := strings.TrimSuffix(name, path.Ext(name))
-		return fmt.Sprintf(fmTemplate, strings.Replace(base, "_", " ", -1))
-		 */
-		return ""
-	}
-
-	linkHandler := func(name string) string {
-		base := strings.TrimSuffix(name, path.Ext(name))
-		return "/" + strings.ToLower(base) + "/"
-	}
-
-	err := doc.GenMarkdownTreeCustom(commands.RootCmd, "./docs", filePrepender, linkHandler)
+	err := doc.GenMarkdownTree(commands.RootCmd, "./")
 	if err != nil {
 		log.Fatal(err)
 	}
