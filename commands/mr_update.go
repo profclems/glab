8437ab2ac24ca55b5b6685b17b45c52d2cc7c8e0
Clement Sam <clementsam75@gmail.com>
2020-08-20 18:23:38 +0000

    feat(mr): add command to mark mr as wip or ready
    Added wip, draft and ready flags to update state of merge request.
    
    Closes #17

--- commands/mr_update.go
+++ commands/mr_update.go
@@ -1,6 +1,7 @@
 package commands
 
 import (
+	"github.com/MakeNowJust/heredoc"
 	"github.com/spf13/cobra"
 	"github.com/xanzy/go-gitlab"
 	"glab/internal/git"
@@ -12,38 +13,69 @@ var mrUpdateCmd = &cobra.Command{
 	Use:   "update <id>",
 	Short: `Update merge requests`,
 	Long:  ``,
+	Example: heredoc.Doc(`
+	$ glab mr update 23 --ready
+	$ glab mr update 23 --draft
+	`),
 	Args:  cobra.ExactArgs(1),
-	Run:   updateMergeRequest,
+	RunE:   updateMergeRequest,
 }
 
-func updateMergeRequest(cmd *cobra.Command, args []string) {
-	if len(args) > 0 {
-		mergeID := strings.Trim(args[0], " ")
-		l := &gitlab.UpdateMergeRequestOptions{}
-		if m, _ := cmd.Flags().GetString("title"); m != "" {
-			l.Title = gitlab.String(m)
-		}
-		if m, _ := cmd.Flags().GetBool("lock-discussion"); m {
-			l.DiscussionLocked = gitlab.Bool(m)
-		}
-		if m, _ := cmd.Flags().GetString("description"); m != "" {
-			l.Description = gitlab.String(m)
-		}
-		gitlabClient, repo := git.InitGitlabClient()
-		if r, _ := cmd.Flags().GetString("repo"); r != "" {
-			repo = r
-		}
-		mr, _, err := gitlabClient.MergeRequests.UpdateMergeRequest(repo, manip.StringToInt(mergeID), l)
-		if err != nil {
-			er(err)
-		}
-		displayMergeRequest(mr)
-	} else {
-		cmdErr(cmd, args)
+func updateMergeRequest(cmd *cobra.Command, args []string) error {
+	mergeID := manip.StringToInt(args[0])
+	l := &gitlab.UpdateMergeRequestOptions{}
+	var mergeTitle string
+	gitlabClient, repo := git.InitGitlabClient()
+	if r, _ := cmd.Flags().GetString("repo"); r != "" {
+		repo = r
 	}
+	isDraft, _ := cmd.Flags().GetBool("draft")
+	isWIP, _ := cmd.Flags().GetBool("wip")
+	if m, _ := cmd.Flags().GetString("title"); m != "" {
+		mergeTitle = m
+	}
+	if mergeTitle == "" {
+		opts := &gitlab.GetMergeRequestsOptions{}
+		mr, _, err := gitlabClient.MergeRequests.GetMergeRequest(repo, mergeID, opts)
+		if err != nil {
+			return err
+		}
+		mergeTitle = mr.Title
+	}
+	if isDraft || isWIP {
+		if isDraft {
+			mergeTitle = "Draft: " + mergeTitle
+		} else {
+			mergeTitle = "WIP: " + mergeTitle
+		}
+	} else if isReady, _ := cmd.Flags().GetBool("ready"); isReady {
+		mergeTitle = strings.TrimPrefix(mergeTitle, "Draft:")
+		mergeTitle = strings.TrimPrefix(mergeTitle, "draft:")
+		mergeTitle = strings.TrimPrefix(mergeTitle, "DRAFT:")
+		mergeTitle = strings.TrimPrefix(mergeTitle, "WIP:")
+		mergeTitle = strings.TrimPrefix(mergeTitle, "wip:")
+		mergeTitle = strings.TrimPrefix(mergeTitle, "Wip:")
+		mergeTitle = strings.TrimSpace(mergeTitle)
+	}
+	l.Title = gitlab.String(mergeTitle)
+	if m, _ := cmd.Flags().GetBool("lock-discussion"); m {
+		l.DiscussionLocked = gitlab.Bool(m)
+	}
+	if m, _ := cmd.Flags().GetString("description"); m != "" {
+		l.Description = gitlab.String(m)
+	}
+	mr, _, err := gitlabClient.MergeRequests.UpdateMergeRequest(repo, mergeID, l)
+	if err != nil {
+		return err
+	}
+	displayMergeRequest(mr)
+	return nil
 }
 
 func init() {
+	mrUpdateCmd.Flags().BoolP("draft", "", false, "Mark merge request as a draft")
+	mrUpdateCmd.Flags().BoolP("ready", "r", false, "Mark merge request as ready to be reviewed and merged")
+	mrUpdateCmd.Flags().BoolP("wip", "", false, "Mark merge request as a work in progress. Alternative to --draft")
 	mrUpdateCmd.Flags().StringP("title", "t", "", "Title of merge request")
 	mrUpdateCmd.Flags().BoolP("lock-discussion", "", false, "Lock discussion on merge request")
 	mrUpdateCmd.Flags().StringP("description", "d", "", "merge request description")
