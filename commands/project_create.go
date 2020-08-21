ed6cd2a4786f81aca3480751454351bfd4ccd691
Prasanna Kumar <prassu158@gmail.com>
2020-08-21 22:14:14 +0530

    feat: Add more attributes to project creations

--- commands/project_create.go
+++ commands/project_create.go
@@ -46,10 +46,17 @@ func runCreateProject(cmd *cobra.Command, args []string) error {
 		visiblity = gitlab.PublicVisibility
 	}
 
+	defaultBranch, _ := cmd.Flags().GetString("defaultBranch")
+	tags, _ := cmd.Flags().GetStringArray("tag")
+	readme, _ := cmd.Flags().GetBool("readme")
+
 	opts := &gitlab.CreateProjectOptions{
-		Name:        gitlab.String(name),
-		Path:        gitlab.String(path),
-		Description: gitlab.String(description),
+		Name:                 gitlab.String(name),
+		Path:                 gitlab.String(path),
+		Description:          gitlab.String(description),
+		DefaultBranch:        gitlab.String(defaultBranch),
+		TagList:              &tags,
+		InitializeWithReadme: gitlab.Bool(readme),
 	}
 
 	if visiblity != "" {
@@ -67,10 +74,13 @@ func runCreateProject(cmd *cobra.Command, args []string) error {
 }
 
 func init() {
-	projectCreateCmd.Flags().StringP("name", "n", "", "name of the new project")
+	projectCreateCmd.Flags().StringP("name", "n", "", "Name of the new project")
 	projectCreateCmd.Flags().StringP("description", "d", "", "Description of the new project")
+	projectCreateCmd.Flags().String("defaultBranch", "", "Default branch of the project. If not provided, `master` by default.")
+	projectCreateCmd.Flags().StringArrayP("tag", "t", []string{}, "The list of tags for the project.")
 	projectCreateCmd.Flags().Bool("internal", false, "Make project internal: visible to any authenticated user (default)")
 	projectCreateCmd.Flags().BoolP("private", "p", false, "Make project private: visible only to project members")
 	projectCreateCmd.Flags().BoolP("public", "P", false, "Make project public: visible without any authentication")
+	projectCreateCmd.Flags().Bool("readme", false, "Initialize project with README.md")
 	projectCmd.AddCommand(projectCreateCmd)
 }
