e69134e8813ee5847d6ab5fa2967b7ca72306540
sanjju simha <sunzoje@gmail.com>
2020-10-04 04:10:40 +0545

    feat: Add Update Sub-command to Issue Command (#240)
    * FEAT: Add Update Sub-command to Issue Command
    
    This PR will add `update` sub-command to `issue` command.
    `update` sub-command edits gitlab issue.
    Currently supported flags are:
    * --label: add labels takes comma separated values
    * --unlabel: remove labels takes comma separated values
    * --title: update title of the issue
    * --description: update descriptions of the issue
    * --lock-discussion: lock comments takes boolean value
    
    * STYLE: Change Spaces to Tab
    
    * STYLE: Change Text on Help Messages
    
    * test: add test to issue update command
    
    Co-authored-by: Clement Sam <clementsam75@gmail.com>

---
+++ commands/issue/update/issue_update_test.go
@@ -0,0 +1,108 @@
+package update
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/google/shlex"
+	"testing"
+	"time"
+
+	"github.com/acarl005/stripansi"
+	"github.com/profclems/glab/commands/cmdtest"
+	"github.com/profclems/glab/pkg/api"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/xanzy/go-gitlab"
+)
+
+func TestNewCmdUpdate(t *testing.T) {
+	t.Parallel()
+
+	oldUpdateIssue := api.UpdateIssue
+	timer, _ := time.Parse(time.RFC3339, "2014-11-12T11:45:26.371Z")
+	testIssue := &gitlab.Issue{
+		ID:          1,
+		IID:         1,
+		State:       "closed",
+		Labels: 	 gitlab.Labels{"bug, test, removeable-label"},
+		Description: "Dummy description for issue 1",
+		DiscussionLocked: false,
+		Author: &gitlab.IssueAuthor{
+			ID:       1,
+			Name:     "John Dev Wick",
+			Username: "jdwick",
+		},
+		CreatedAt: &timer,
+	}
+	api.UpdateIssue = func(client *gitlab.Client, projectID interface{}, issueID int, opts *gitlab.UpdateIssueOptions) (*gitlab.Issue, error) {
+		if projectID == "" || projectID == "WRONG_REPO" || projectID == "expected_err" || issueID != testIssue.ID {
+			return nil, fmt.Errorf("error expected")
+		}
+		if *opts.Title != "" {
+			testIssue.Title = *opts.Title
+		}
+		if *opts.Description != "" {
+			testIssue.Description = *opts.Description
+		}
+		if opts.AddLabels != nil {
+			testIssue.Labels = opts.AddLabels
+		}
+		return testIssue, nil
+	}
+
+	testCases := []struct {
+		Name        string
+		Issue       string
+		ExpectedMsg []string
+		wantErr     bool
+	}{
+		{
+			Name:        "Issue Exists",
+			Issue:       `1 -t "New Title" -d "A new description" --lock-discussion -l newLabel --unlabel bug`,
+			ExpectedMsg: []string{"- Updating issue #1", "✓ Updated", "#1 New Title"},
+		},
+		{
+			Name:        "Issue Exists on different repo",
+			Issue:       `1 -R glab_cli/test`,
+			ExpectedMsg: []string{"- Updating issue #1", "✓ Updated"},
+		},
+		{
+			Name:        "Issue Does Not Exist",
+			Issue:       "0",
+			ExpectedMsg: []string{"- Updating issue #0", "error expected"},
+			wantErr:     true,
+		},
+	}
+
+	cmd := NewCmdUpdate(cmdtest.StubFactory("https://gitlab.com/glab-cli/test"))
+	cmd.Flags().StringP("repo", "R", "", "")
+
+	for _, tc := range testCases {
+		t.Run(tc.Name, func(t *testing.T) {
+			var stderr bytes.Buffer
+			var stdout bytes.Buffer
+
+			args, _ := shlex.Split(tc.Issue)
+			cmd.SetArgs(args)
+			cmd.SetOut(&stdout)
+			cmd.SetErr(&stderr)
+
+			_, err := cmd.ExecuteC()
+			if tc.wantErr {
+				require.Error(t, err)
+				return
+			} else {
+				require.NoError(t, err)
+			}
+
+			out := stripansi.Strip(stdout.String())
+			//outErr := stripansi.Strip(stderr.String())
+
+			for _, msg := range tc.ExpectedMsg {
+				assert.Contains(t, out, msg)
+			}
+		})
+	}
+
+	api.UpdateIssue = oldUpdateIssue
+}
