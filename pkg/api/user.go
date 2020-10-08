3f14762b04e978cd812bb6a9d287b6cd367a606f
Alberts Zemzale <alberts.zemzale@gmail.com>
2020-10-08 07:56:07 +0300

    feat: Add UserByName api function
    To get MRs for assignees we first need to get the user object itself.
    From there we can get the user ID that is needed for fetching MRs by
    assignee.

--- pkg/api/user.go
+++ pkg/api/user.go
@@ -1,6 +1,9 @@
 package api
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/xanzy/go-gitlab"
 )
 
@@ -13,4 +16,18 @@ var CurrentUser = func(client *gitlab.Client) (*gitlab.User, error) {
 		return nil, err
 	}
 	return u, nil
+}
+
+var UserByName = func(client *gitlab.Client, name string) (*gitlab.User, error) {
+	opts := &gitlab.ListUsersOptions{Username: gitlab.String(name)}
+	users, _, err := apiClient.Users.ListUsers(opts)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(users) != 1 {
+		return nil, errors.New(fmt.Sprintf("failed to find user by name : %s", name))
+	}
+
+	return users[0], nil
 }
