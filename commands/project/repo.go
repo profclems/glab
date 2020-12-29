5e2e549e19f369bb3af02feafad59930a6d26315
Peter Benjamin <petermbenjamin@gmail.com>
2020-12-29 15:36:18 -0800

    feat(repo): implement web flag

--- commands/project/repo.go
+++ commands/project/repo.go
@@ -1,6 +1,8 @@
 package project
 
 import (
+	"fmt"
+
 	"github.com/profclems/glab/commands/cmdutils"
 	repoCmdArchive "github.com/profclems/glab/commands/project/archive"
 	repoCmdClone "github.com/profclems/glab/commands/project/clone"
@@ -9,16 +11,40 @@ import (
 	repoCmdDelete "github.com/profclems/glab/commands/project/delete"
 	repoCmdFork "github.com/profclems/glab/commands/project/fork"
 	repoCmdSearch "github.com/profclems/glab/commands/project/search"
+	"github.com/profclems/glab/internal/utils"
 
 	"github.com/spf13/cobra"
 )
 
+type RepoOpts struct {
+	OpenInBrowser bool
+}
+
 func NewCmdRepo(f *cmdutils.Factory) *cobra.Command {
+	opts := RepoOpts{}
 	var repoCmd = &cobra.Command{
 		Use:     "repo <command> [flags]",
 		Short:   `Work with GitLab repositories and projects`,
 		Long:    ``,
 		Aliases: []string{"project"},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cfg, err := f.Config()
+			if err != nil {
+				return err
+			}
+			baseRepo, err := f.BaseRepo()
+			if err != nil {
+				return err
+			}
+			if opts.OpenInBrowser {
+				browser, err := cfg.Get(baseRepo.RepoHost(), "browser")
+				if err != nil {
+					return err
+				}
+				return utils.OpenInBrowser(fmt.Sprintf("https://%s/%s", baseRepo.RepoHost(), baseRepo.FullName()), browser)
+			}
+			return nil
+		},
 	}
 
 	repoCmd.AddCommand(repoCmdArchive.NewCmdArchive(f))
@@ -28,6 +54,8 @@ func NewCmdRepo(f *cmdutils.Factory) *cobra.Command {
 	repoCmd.AddCommand(repoCmdDelete.NewCmdDelete(f))
 	repoCmd.AddCommand(repoCmdFork.NewCmdFork(f, nil))
 	repoCmd.AddCommand(repoCmdSearch.NewCmdSearch(f))
+
+	repoCmd.Flags().BoolVarP(&opts.OpenInBrowser, "web", "w", false, "Open repo in a browser. Uses default browser or browser specified in BROWSER variable")
 
 	return repoCmd
 }
