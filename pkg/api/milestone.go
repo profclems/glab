b10feb9d9113261a29520974a8afcc000ca5ba04
Leo <thinkabit.ukim@gmail.com>
2020-12-30 13:49:05 -0300

    feat(pkg/api/milestone): implement finding Milestones by its title
    This is a building block required for us to support passing --milestone
    <string> instead of asking the user for the global ID of the milestone

--- pkg/api/milestone.go
+++ pkg/api/milestone.go
@@ -1,6 +1,10 @@
 package api
 
-import "github.com/xanzy/go-gitlab"
+import (
+	"fmt"
+
+	"github.com/xanzy/go-gitlab"
+)
 
 var ListMilestones = func(client *gitlab.Client, projectID interface{}, opts *gitlab.ListMilestonesOptions) ([]*gitlab.Milestone, error) {
 	if client == nil {
@@ -16,4 +20,27 @@ var ListMilestones = func(client *gitlab.Client, projectID interface{}, opts *gi
 		return nil, err
 	}
 	return milestone, nil
+}
+
+var MilestoneByTitle = func(client *gitlab.Client, projectID interface{}, name string) (*gitlab.Milestone, error) {
+	opts := &gitlab.ListMilestonesOptions{Title: gitlab.String(name)}
+
+	if client == nil {
+		client = apiClient.Lab()
+	}
+
+	if opts.PerPage == 0 {
+		opts.PerPage = DefaultListLimit
+	}
+
+	milestones, _, err := client.Milestones.ListMilestones(projectID, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(milestones) != 1 {
+		return nil, fmt.Errorf("failed to find milestone by title: %s", name)
+	}
+
+	return milestones[0], nil
 }
