7d15f21c5a8584142ebff9021426bf781575c9fa
Leo <thinkabit.ukim@gmail.com>
2020-12-31 06:53:18 -0300

    feat(commands/issue/update): add --assign and --unassign flags

--- commands/issue/update/issue_update.go
+++ commands/issue/update/issue_update.go
@@ -80,6 +80,56 @@ func NewCmdUpdate(f *cmdutils.Factory) *cobra.Command {
 				l.MilestoneID = gitlab.Int(mID)
 			}
 
+			if unassign, _ := cmd.Flags().GetStringSlice("unassign"); len(unassign) > 0 {
+				// Get the issue
+				issue, err := api.GetIssue(apiClient, repo.FullName(), issueID)
+				if err != nil {
+					return err
+				}
+
+				// Store the ID of all already assigned users
+				var assignedUsers []int
+				for i := range issue.Assignees {
+					assignedUsers = append(assignedUsers, issue.Assignees[i].ID)
+				}
+
+				users, err := api.UsersByNames(apiClient, unassign)
+				if err != nil {
+					return err
+				}
+				usersToUnassign := cmdutils.IDsFromUsers(users)
+
+				// Check every ID of assignedUsers against usersToUnassign and if any ID from usersToUnassign
+				// matches any from usersToUnassign, don't add it to l.AssigneeIDs
+				for _, userID := range assignedUsers {
+					if !utils.IsInSliceInt(usersToUnassign, userID) {
+						l.AssigneeIDs = append(l.AssigneeIDs, userID)
+					}
+				}
+			}
+
+			if assign, _ := cmd.Flags().GetStringSlice("assign"); len(assign) > 0 {
+				// Get the issue
+				issue, err := api.GetIssue(apiClient, repo.FullName(), issueID)
+				if err != nil {
+					return err
+				}
+
+				// If the unassign flag was used then re-use l.AssigneeIDs, otherwise get the
+				// IDs from the already assigned users
+				if ok := cmd.Flags().Changed("unassign"); !ok {
+					var assignedUsers []int
+					for i := range issue.Assignees {
+						l.AssigneeIDs = append(assignedUsers, issue.Assignees[i].ID)
+					}
+				}
+
+				users, err := api.UsersByNames(apiClient, assign)
+				if err != nil {
+					return err
+				}
+				l.AssigneeIDs = append(l.AssigneeIDs, cmdutils.IDsFromUsers(users)...)
+			}
 
 			fmt.Fprintf(out, "- Updating issue #%d\n", issueID)
 
@@ -104,6 +154,8 @@ func NewCmdUpdate(f *cmdutils.Factory) *cobra.Command {
 	issueUpdateCmd.Flags().BoolP("public", "p", false, "Make issue public")
 	issueUpdateCmd.Flags().BoolP("confidential", "c", false, "Make issue confidential")
 	issueUpdateCmd.Flags().StringP("milestone", "m", "", "title of the milestone to assign")
+	issueUpdateCmd.Flags().StringSliceP("assign", "a", []string{}, "Assign new users to the issue by their `usernames`, overrides --unassign")
+	issueUpdateCmd.Flags().StringSliceP("unassign", "", []string{}, "Unassign users from the issue by their `usernames`")
 
 	return issueUpdateCmd
 }
