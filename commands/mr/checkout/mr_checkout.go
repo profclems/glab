092cdd8a6e7a2d2a249e2d2b817729060065c11b
Leo <thinkabit.ukim@gmail.com>
2021-01-08 21:09:46 -0300

    feat(commands/mr/checkout): add --set-upstream-to flag

--- commands/mr/checkout/mr_checkout.go
+++ commands/mr/checkout/mr_checkout.go
@@ -1,7 +1,9 @@
 package checkout
 
 import (
+	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/MakeNowJust/heredoc"
 	"github.com/profclems/glab/commands/cmdutils"
@@ -14,8 +16,9 @@ import (
 )
 
 type mrCheckoutConfig struct {
-	branch string
-	track  bool
+	branch   string
+	track    bool
+	upstream string
 }
 
 var (
@@ -31,12 +34,34 @@ func NewCmdCheckout(f *cmdutils.Factory) *cobra.Command {
 			$ glab mr checkout 1
 			$ glab mr checkout branch --track
 			$ glab mr checkout 12 --branch todo-fix
+			$ glab mr checkout new-feature --set-upstream-to=upstream/trunk
 			$ glab mr checkout   # use checked out branch
 		`),
 		Args: cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-
 			var err error
+			var upstream string
+
+			if mrCheckoutCfg.upstream != "" {
+				// Make sure we don't have the mutually exclusive flags --track and --set-upstream-to
+				if mrCheckoutCfg.track {
+					return &cmdutils.FlagError{Err: errors.New("--track and --set-upstream-to are mutually exclusive")}
+				}
+
+				upstream = mrCheckoutCfg.upstream
+
+				if val := strings.Split(mrCheckoutCfg.upstream, "/"); len(val) > 1 {
+					// Verify that we have the remote set
+					repo, err := f.Remotes()
+					if err != nil {
+						return err
+					}
+					_, err = repo.FindByName(val[0])
+					if err != nil {
+						return err
+					}
+				}
+			}
 
 			apiClient, err := f.HttpClient()
 			if err != nil {
@@ -91,11 +116,18 @@ func NewCmdCheckout(f *cmdutils.Factory) *cobra.Command {
 			if err := git.CheckoutBranch(mrCheckoutCfg.branch); err != nil {
 				return err
 			}
+
+			// Check out the branch
+			if upstream != "" {
+				if err := git.RunCmd([]string{"branch", "--set-upstream-to", upstream}); err != nil {
+					return err
+				}
+			}
 			return nil
 		},
 	}
 	mrCheckoutCmd.Flags().StringVarP(&mrCheckoutCfg.branch, "branch", "b", "", "checkout merge request with <branch> name")
 	mrCheckoutCmd.Flags().BoolVarP(&mrCheckoutCfg.track, "track", "t", false, "set checked out branch to track remote branch, adds remote if needed")
-
+	mrCheckoutCmd.Flags().StringVarP(&mrCheckoutCfg.upstream, "set-upstream-to", "u", "", "set tracking of checked out branch to [REMOTE/]BRANCH")
 	return mrCheckoutCmd
 }
