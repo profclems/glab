d52b76a779e5f20678dfd1e1e769949204e61dc9
Clement Sam <clementsam75@gmail.com>
2021-01-15 14:37:16 +0000

    feat: add auth status command

--- commands/auth/auth.go
+++ commands/auth/auth.go
@@ -2,6 +2,7 @@ package auth
 
 import (
 	authLoginCmd "github.com/profclems/glab/commands/auth/login"
+	authStatusCmd "github.com/profclems/glab/commands/auth/status"
 	"github.com/profclems/glab/commands/cmdutils"
 	"github.com/spf13/cobra"
 )
@@ -13,6 +14,7 @@ func NewCmdAuth(f *cmdutils.Factory) *cobra.Command {
 	}
 
 	cmd.AddCommand(authLoginCmd.NewCmdLogin(f))
+	cmd.AddCommand(authStatusCmd.NewCmdStatus(f, nil))
 
 	return cmd
 }
