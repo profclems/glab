6c04d032f102832875212ded99b4ea29d9dd0707
Leo <thinkabit.ukim@gmail.com>
2021-02-17 22:26:20 -0300

    refactor(commands/mr/rebase): handle state changes
    we can only rebase "opened" merge requests

--- commands/mr/rebase/mr_rebase.go
+++ commands/mr/rebase/mr_rebase.go
@@ -32,7 +32,7 @@ func NewCmdRebase(f *cmdutils.Factory) *cobra.Command {
 				return err
 			}
 
-			mr, repo, err := mrutils.MRFromArgs(f, args)
+			mr, repo, err := mrutils.MRFromArgs(f, args, "opened")
 			if err != nil {
 				return err
 			}
