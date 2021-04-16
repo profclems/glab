5b2e065a89fb9efc77b73a55ace5fb62d1acac38
Brendan O'Leary <boleary@gitlab.com>
2021-04-15 22:46:53 -0400

    Add repo list

--- commands/project/repo.go
+++ commands/project/repo.go
@@ -8,6 +8,7 @@ import (
 	repoCmdCreate "github.com/profclems/glab/commands/project/create"
 	repoCmdDelete "github.com/profclems/glab/commands/project/delete"
 	repoCmdFork "github.com/profclems/glab/commands/project/fork"
+	repoCmdList "github.com/profclems/glab/commands/project/list"
 	repoCmdSearch "github.com/profclems/glab/commands/project/search"
 	repoCmdView "github.com/profclems/glab/commands/project/view"
 
@@ -25,6 +26,7 @@ func NewCmdRepo(f *cmdutils.Factory) *cobra.Command {
 	repoCmd.AddCommand(repoCmdArchive.NewCmdArchive(f))
 	repoCmd.AddCommand(repoCmdClone.NewCmdClone(f, nil))
 	repoCmd.AddCommand(repoCmdContributors.NewCmdContributors(f))
+	repoCmd.AddCommand(repoCmdList.NewCmdList(f))
 	repoCmd.AddCommand(repoCmdCreate.NewCmdCreate(f))
 	repoCmd.AddCommand(repoCmdDelete.NewCmdDelete(f))
 	repoCmd.AddCommand(repoCmdFork.NewCmdFork(f, nil))
