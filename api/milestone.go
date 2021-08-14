0cf8e611035bd837e26bdf698adb40c76d043472
Alberts Zemzale <alberts.zemzale@gmail.com>
2021-08-14 21:06:35 +0300

    feat(cmdutils): Add group milestones to the prompt
    When creating issues we prompted only project milestones. This would
    break workflows where group milestones were used.
    
    They are two different APIs so we have to normalize their output and
    merge it to one.
    
    Issue #698

--- api/milestone.go
+++ api/milestone.go
@@ -4,9 +4,87 @@ import (
 	"fmt"
 
 	"github.com/xanzy/go-gitlab"
+	"golang.org/x/sync/errgroup"
 )
 
-var ListMilestones = func(client *gitlab.Client, projectID interface{}, opts *gitlab.ListMilestonesOptions) ([]*gitlab.Milestone, error) {
+// Describe namespace kinds which is either group or user
+// See docs: https://docs.gitlab.com/ee/api/namespaces.html
+const (
+	NamespaceKindUser  = "user"
+	NamespaceKindGroup = "group"
+)
+
+type Milestone struct {
+	ID    int
+	Title string
+}
+
+func NewProjectMilestone(m *gitlab.Milestone) *Milestone {
+	return &Milestone{
+		ID:    m.ID,
+		Title: m.Title,
+	}
+}
+
+func NewGroupMilestone(m *gitlab.GroupMilestone) *Milestone {
+	return &Milestone{
+		ID:    m.ID,
+		Title: m.Title,
+	}
+}
+
+type ListMilestonesOptions struct {
+	IIDs                    []int
+	State                   *string
+	Title                   *string
+	Search                  *string
+	IncludeParentMilestones *bool
+	PerPage                 int
+	Page                    int
+}
+
+func (opts *ListMilestonesOptions) ListProjectMilestonesOptions() *gitlab.ListMilestonesOptions {
+	projectOpts := &gitlab.ListMilestonesOptions{
+		IIDs:   opts.IIDs,
+		State:  opts.State,
+		Title:  opts.Title,
+		Search: opts.Search,
+	}
+	projectOpts.PerPage = opts.PerPage
+	projectOpts.Page = opts.Page
+	return projectOpts
+}
+
+func (opts *ListMilestonesOptions) ListGroupMilestonesOptions() *gitlab.ListGroupMilestonesOptions {
+	groupOpts := &gitlab.ListGroupMilestonesOptions{
+		IIDs:                    opts.IIDs,
+		State:                   opts.State,
+		Title:                   opts.Title,
+		Search:                  opts.Search,
+		IncludeParentMilestones: opts.IncludeParentMilestones,
+	}
+	groupOpts.PerPage = opts.PerPage
+	groupOpts.Page = opts.Page
+	return groupOpts
+}
+
+var ListGroupMilestones = func(client *gitlab.Client, groupID interface{}, opts *gitlab.ListGroupMilestonesOptions) ([]*gitlab.GroupMilestone, error) {
+	if client == nil {
+		client = apiClient.Lab()
+	}
+
+	if opts.PerPage == 0 {
+		opts.PerPage = DefaultListLimit
+	}
+
+	milestone, _, err := client.GroupMilestones.ListGroupMilestones(groupID, opts)
+	if err != nil {
+		return nil, err
+	}
+	return milestone, nil
+}
+
+var ListProjectMilestones = func(client *gitlab.Client, projectID interface{}, opts *gitlab.ListMilestonesOptions) ([]*gitlab.Milestone, error) {
 	if client == nil {
 		client = apiClient.Lab()
 	}
@@ -22,7 +100,7 @@ var ListMilestones = func(client *gitlab.Client, projectID interface{}, opts *gi
 	return milestone, nil
 }
 
-var MilestoneByTitle = func(client *gitlab.Client, projectID interface{}, name string) (*gitlab.Milestone, error) {
+var ProjectMilestoneByTitle = func(client *gitlab.Client, projectID interface{}, name string) (*gitlab.Milestone, error) {
 	opts := &gitlab.ListMilestonesOptions{Title: gitlab.String(name)}
 
 	if client == nil {
@@ -43,4 +121,44 @@ var MilestoneByTitle = func(client *gitlab.Client, projectID interface{}, name s
 	}
 
 	return milestones[0], nil
+}
+
+var ListAllMilestones = func(client *gitlab.Client, projectID interface{}, opts *ListMilestonesOptions) ([]*Milestone, error) {
+	project, err := GetProject(client, projectID)
+	if err != nil {
+		return nil, err
+	}
+
+	errGroup := &errgroup.Group{}
+	projectMilestones := []*gitlab.Milestone{}
+	groupMilestones := []*gitlab.GroupMilestone{}
+
+	errGroup.Go(func() error {
+		var err error
+		projectMilestones, err = ListProjectMilestones(client, projectID, opts.ListProjectMilestonesOptions())
+		return err
+	})
+
+	if project.Namespace.Kind == NamespaceKindGroup {
+		errGroup.Go(func() error {
+			var err error
+			groupMilestones, err = ListGroupMilestones(client, project.Namespace.ID, opts.ListGroupMilestonesOptions())
+			return err
+		})
+	}
+
+	if err := errGroup.Wait(); err != nil {
+		return nil, fmt.Errorf("failed to get all project related milestones. %w", err)
+	}
+
+	milestones := make([]*Milestone, 0, len(projectMilestones)+len(groupMilestones))
+	for _, v := range projectMilestones {
+		milestones = append(milestones, NewProjectMilestone(v))
+	}
+
+	for _, v := range groupMilestones {
+		milestones = append(milestones, NewGroupMilestone(v))
+	}
+
+	return milestones, nil
 }
