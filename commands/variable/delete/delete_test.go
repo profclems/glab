edb987cddd28db0418a656184a519145f4d1af9d
Brady Mitchell <bradym@users.noreply.github.com>
2021-10-28 21:13:42 -0600

    working tests

--- commands/variable/delete/delete_test.go
+++ commands/variable/delete/delete_test.go
@@ -87,33 +87,85 @@ func Test_NewCmdSet(t *testing.T) {
 	}
 }
 
-func Test_deleteRun_project(t *testing.T) {
+func Test_deleteRun(t *testing.T) {
 	reg := &httpmock.Mocker{
 		MatchURL: httpmock.PathAndQuerystring,
 	}
 	defer reg.Verify(t)
 
-	reg.RegisterResponder("DELETE", "projects/owner%2Frepo/variables/TEST_VAR?filter%5Benvironment_scope%5D=%2A",
-		httpmock.NewStringResponse(204, ""),
+	reg.RegisterResponder("DELETE", "/api/v4/projects/owner%2Frepo/variables/TEST_VAR?filter%5Benvironment_scope%5D=%2A",
+		httpmock.NewStringResponse(204, " "),
 	)
 
-	io, _, stdout, _ := iostreams.Test()
+	reg.RegisterResponder("DELETE", "/api/v4/projects/owner%2Frepo/variables/TEST_VAR?filter%5Benvironment_scope%5D=stage",
+		httpmock.NewStringResponse(204, " "),
+	)
 
-	opts := &DeleteOpts{
-		HTTPClient: func() (*gitlab.Client, error) {
-			a, _ := api.TestClient(&http.Client{Transport: reg}, "", "gitlab.com", false)
-			return a.Lab(), nil
-		},
-		BaseRepo: func() (glrepo.Interface, error) {
-			return glrepo.FromFullName("owner/repo")
-		},
-		IO:    io,
-		Key:   "TEST_VAR",
-		Scope: "*",
+	reg.RegisterResponder("DELETE", "/api/v4/groups/testGroup/variables/TEST_VAR",
+		httpmock.NewStringResponse(204, " "),
+	)
+
+	var httpClient = func() (*gitlab.Client, error) {
+		a, _ := api.TestClient(&http.Client{Transport: reg}, "", "gitlab.com", false)
+		return a.Lab(), nil
 	}
-	_, _ = opts.HTTPClient()
+	var baseRepo = func() (glrepo.Interface, error) {
+		return glrepo.FromFullName("owner/repo")
+	}
 
-	err := deleteRun(opts)
-	assert.NoError(t, err)
-	assert.Equal(t, stdout.String(), "✓ Deleted variable TEST_VAR with scope * for owner/repo\n")
+	tests := []struct {
+		name        string
+		opts        DeleteOpts
+		wantsErr    bool
+		wantsOutput string
+	}{
+		{
+			name: "delete project variable no scope",
+			opts: DeleteOpts{
+				HTTPClient: httpClient,
+				BaseRepo:   baseRepo,
+				Key:        "TEST_VAR",
+				Scope:      "*",
+			},
+			wantsErr:    false,
+			wantsOutput: "✓ Deleted variable TEST_VAR with scope * for owner/repo\n",
+		},
+		{
+			name: "delete project variable with stage scope",
+			opts: DeleteOpts{
+				HTTPClient: httpClient,
+				BaseRepo:   baseRepo,
+				Key:        "TEST_VAR",
+				Scope:      "stage",
+			},
+			wantsErr:    false,
+			wantsOutput: "✓ Deleted variable TEST_VAR with scope stage for owner/repo\n",
+		},
+		{
+			name: "delete group variable",
+			opts: DeleteOpts{
+				HTTPClient: httpClient,
+				BaseRepo:   baseRepo,
+				Key:        "TEST_VAR",
+				Scope:      "",
+				Group:      "testGroup",
+			},
+			wantsErr:    false,
+			wantsOutput: "✓ Deleted variable TEST_VAR for group testGroup\n",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, _ = tt.opts.HTTPClient()
+
+			io, _, stdout, _ := iostreams.Test()
+			tt.opts.IO = io
+			io.IsInTTY = false
+
+			err := deleteRun(&tt.opts)
+			assert.NoError(t, err)
+			assert.Equal(t, stdout.String(), tt.wantsOutput)
+		})
+	}
 }
