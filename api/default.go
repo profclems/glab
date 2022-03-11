c05f1036c840c1dd9f42e56728d1d218b1e84660
Eduardo Cardoso <eduardo.gil.s.cardoso@gmail.com>
2022-03-10 22:38:58 -0300

    fix: fix validation for GitLab 14.5 tokens (#971)

--- api/default.go
+++ api/default.go
@@ -3,10 +3,10 @@ package api
 var DefaultListLimit = 30
 
 //IsValidToken checks if a token provided is valid.
-// The token string must be 20 characters in length to be recognized as
-// a valid personal access token.
+// The token string must be 26 characters in length and have the 'glpat-'
+// prefix or just be 20 characters long to be recognized as a valid personal access token.
 //
 // TODO: check if token has minimum scopes required by glab
 func IsValidToken(token string) bool {
-	return len(token) == 20
+	return (len(token) == 26 && token[:6] == "glpat-") || len(token) == 20
 }
